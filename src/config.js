3f0bdebfd6e0990031d73be2b20f661b001ef7a0
sb48 <sb48@sanger.ac.uk>
2019-01-03 09:48:37 +0000

    Fix typo in config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ import {authenticators} from './constants'
 
 console.log(process.env);
 export const api_url = process.env.REACT_APP_API_URL;
-export const authenticator = authenticators[process.env.PAGESMITH_TEST];
+export const authenticator = authenticators[process.env.REACT_APP_AUTHENTICATOR];
 export const developer = process.env.REACT_APP_DEVELOPER === "true";
