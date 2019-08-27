ab855c8c62627544aa87e363c8f9b6efc22739b9
Josh Holland <jh36@sanger.ac.uk>
2019-08-27 14:35:51 +0000

    Display feedback submission links to Graduate Office

--- src/components/project.js
+++ src/components/project.js
@@ -91,11 +91,11 @@ class Project extends Component {
             }
         }
         if (project.grace_passed) {
-            if ((user.id === project.supervisor_id)
+            if ((user.id === project.supervisor_id || user.permissions.modify_permissions)
                 && project.supervisor_feedback_id == null) {
                 links.push(<div key="provide_feedback_supervisor"><h3><Link to={`/projects/${project.id}/provide_feedback/supervisor`}>Provide Supervisor Feedback</Link></h3></div>);
             }
-            if ((user.id === project.cogs_marker_id)
+            if ((user.id === project.cogs_marker_id || user.permissions.modify_permissions)
                 && project.cogs_feedback_id == null) {
                 links.push(<div key="provide_feedback_cogs"><h3><Link to={`/projects/${project.id}/provide_feedback/cogs`}>Provide CoGS Feedback</Link></h3></div>);
             }
